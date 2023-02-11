a75609d490f34a4311c3180673c3d57fd6a6f0e9
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2023-02-10 22:25:27 -0500

    Update controller.ts

--- source/components/restaurante/controller.ts
+++ source/components/restaurante/controller.ts
@@ -41,7 +41,7 @@ export const AllRestaurante = async (_req: Request, res: Response): Promise<void
       select: {
         nombre:true,departamento:true,telefono:true,referencia:true,distrito:true
         ,provincia:true,usuarioId:true,
-        tipo:true,apertura:true,cierre:true,fech_creacion:true,calificacion:true,descripcion:true
+        tipo:true,apertura:true,cierre:true,fech_creacion:true,calificacion:true,descripcion:true,restaurante_id :true
       },
     });
     res.json(allrestau);
